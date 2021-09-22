ebd83622bc7c07eff2f697c6f7c28243850c4cf3
Luis Rojas <larr014@gmail.com>
2021-09-22 12:33:49 -0300

    Clase 9

--- Tienda/Empresa.py
+++ Tienda/Empresa.py
@@ -1,4 +1,5 @@
 from Empleado import *
+from Cliente import *
 class Empresa:
 
 
@@ -8,6 +9,11 @@ class Empresa:
         self.__empleados = [] #Una empresa tiene una lista de empleados
         self.__clientes = [] #Una empresa tiene una lista de clientes
 
+    '''Si es un rombo negro entre empleado y empresa, si empresa es el todo
+        el empleado no puede existir si no existe la empresa 
+
+        .- Debe crearse el empleado dentro de la empresa
+    '''
     def agregar_empleado(self):
         e = Empleado() #Creamos un empleado 
         self.__empleados.append(e) #Agregamos a la lista
@@ -15,5 +21,18 @@ class Empresa:
     def mostrar_empleados(self):
         for e in self.__empleados: #Por cada empleado de la lista
             e.mostrar() #Muestrame sus datos
-    
 
+    '''Si es un rombro blanco entre cliente y empresa, si empresa es el todo
+    el cliente si puede existir si no existe la empresa
+    
+    .- Debe crearse el cliente fuera de la empresa'''
+    def agregar_cliente(self,cliente):
+        self.__clientes.append(cliente)
+
+    def mostrar_clientes(self):
+        for c in self.__clientes: #Por cada cliente de la lista
+            c.mostrar() #Muestra sus datos
+    
+    def mostrar(self):
+        print(f"Nombre: {self.__nombre}")
+        
--- Tienda/main.py
+++ Tienda/main.py
@@ -1,14 +1,47 @@
 from Empleado import *
 from Empresa import *
-e1 = Empresa("Cinemarx")
-e2 = Empresa("Unimarx")
-print("Agregando empleado empresa 1")
-e1.agregar_empleado()
-e1.agregar_empleado()
-print("Agregando empleado empresa 2")
-e2.agregar_empleado()
-print("Mostrando empleados empresa 1")
-e1.mostrar_empleados()
-print("Mostrando empleados empresa 2")
-e2.mostrar_empleados()
+clientes = [] #Lista global de clientes
+empresas = [] #Lista global de empresas
+
+def agregar_empresa():
+    nombre = input("Ingrese nombre de la empresa: ")
+    empresa = Empresa(nombre) 
+    empresas.append(empresa)
+
+def mostrar_empresas():
+    contador = 0
+    print('----')
+    for e in empresas:
+        
+        contador += 1
+        print(f'{contador}',end=" ")
+        e.mostrar()
+    print('----')
+
+def eliminar_empresa():
+    mostrar_empresas()
+    id = int(input("Ingrese id de la empresa a eliminar: "))
+    empresas.pop(id-1)
+
+  
+#Ciclo infinito
+while True:
+    print('1.- Agregar Empresa')
+    print('2.- Mostrar Empresas')
+    print('3.- Eliminar Empresa')
+    print('4.- Agregar Cliente')
+    print('5.- Mostrar Clientes')
+    print('6.- Eliminar Clientes')
+    opcion = input("Ingrese una opcion: ")
+    if opcion=="1":
+        agregar_empresa()
+    elif opcion=="2":
+        mostrar_empresas()
+    elif opcion=="3":
+        eliminar_empresa()
+
+
+
+
+
 
